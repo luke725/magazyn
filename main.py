c71a3cbc04fb2a0f6f72ff58e83448146ccc8549
Lukasz Wolochowski <l.wolochowski@students.mimuw.edu.pl>
2016-02-18 09:07:37 +0100

    Already written files.

---
+++ main.py
@@ -0,0 +1,85 @@
+import sys
+from PyQt4 import QtCore, QtGui
+from main_window import Ui_MainWindow
+from model import *
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+      QtGui.QWidget.__init__(self, parent)
+      self.ui = Ui_MainWindow()
+      self.ui.setupUi(self)
+      self.ui.equipmentUsedLineEdit.setValidator(QtGui.QIntValidator(0, 99999999999, self))
+      self.ui.addUsageButton.clicked.connect(self.add_usage)
+      self.ui.tableWidget_2.itemChanged.connect(self.change_equipment_amount)
+      
+    def change_equipment_amount(self, item):
+      if (item.text().toInt()[1]):
+        amount_available = item.text().toInt()[0]
+        equipment_id = self.ui.tableWidget_2.item(item.row(), 0).text().toInt()[0]
+        update_equipment(equipment_id, amount_available)
+      self.draw()
+      
+      
+    def add_usage(self):
+      equipment_id = self.ui.equipmentUsedLineEdit.text().toInt()[0]
+      usage = self.ui.amountUsedSpinBox.value()
+      self.ui.equipmentUsedLineEdit.clear()
+      self.ui.amountUsedSpinBox.setValue(1)
+      add_usage(equipment_id, usage)
+      self.draw()
+
+      
+    def __set_row(self, row, key, value):
+      self.ui.tableWidget.setItem(row, 0, self.create_read_only_table_widget_item(str(key)))
+      self.ui.tableWidget.setItem(row, 1, self.create_read_only_table_widget_item(str(value)))
+      
+    def __set_row_2(self, row, key, value):
+      self.ui.tableWidget_2.setItem(row, 0, self.create_read_only_table_widget_item(str(key)))
+      self.ui.tableWidget_2.setItem(row, 1, self.create_table_widget_item(str(value)))
+      
+    def __draw_usage(self, session):
+      self.ui.tableWidget.clearContents()
+      current_treatment = session.get_current_or_create()
+      
+      row_count = len(current_treatment.equipment_usages)
+      self.ui.tableWidget.setRowCount(row_count)
+      for row in range(row_count):
+        usage = current_treatment.equipment_usages[row]
+        self.__set_row(row, usage.equipment_id, usage.amount_used)
+      
+    def __draw_state(self, session):
+      self.ui.tableWidget_2.blockSignals(True)
+      self.ui.tableWidget_2.clearContents()
+      all_equipment = session.all_equipment()
+      
+      row_count = len(all_equipment)
+      self.ui.tableWidget_2.setRowCount(row_count)
+      for row in range(row_count):
+        usage = all_equipment[row]
+        self.__set_row_2(row, usage.id, usage.amount_available)
+      self.ui.tableWidget_2.blockSignals(False)
+      
+      
+    def draw(self):
+      session = Session()
+      self.__draw_usage(session)
+      self.__draw_state(session)
+      session.close()
+
+    def create_read_only_table_widget_item(self, string):  
+      item = QtGui.QTableWidgetItem(string)
+      item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
+      return item
+  
+    def create_table_widget_item(self, string):
+      item = QtGui.QTableWidgetItem(string)
+      return item
+      
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = MainWindow()
+    myapp.show()
+    myapp.draw()
+    sys.exit(app.exec_())
---
+++ model.py
@@ -0,0 +1,134 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relation, sessionmaker, relationship
+
+Base = declarative_base()
+
+class Treatment(Base):
+  __tablename__ = 'treatments'
+
+  id = Column(Integer, primary_key=True)
+  is_current = Column(Boolean, nullable=False)
+
+  equipment_usages = relationship("EquipmentUsage", back_populates="treatment")
+
+  def __init__(self, is_current=False):
+    self.is_current = is_current
+
+  def __repr__(self):
+    return "Treatment(%d)" % (self.id)
+    
+  def find_usage(self, equipment_id):
+    for usage in self.equipment_usages:
+      if usage.equipment_id == equipment_id:
+        return usage
+    return None
+    
+  def add_usage(self, equipment_id, amount=0):
+    usage = self.find_usage(equipment_id)
+    if usage is not None:
+      usage.amount_used += amount
+    else:
+      usage = EquipmentUsage(equipment_id=equipment_id, amount_used=amount)
+      self.equipment_usages.append(usage)
+
+class Equipment(Base):
+  __tablename__ = 'equipments'
+
+  id = Column(Integer, primary_key=True)
+  amount_available = Column(Integer, nullable=False)
+  
+  equipment_usages = relationship("EquipmentUsage", back_populates="equipment")
+
+  def __init__(self, id, amount_available=0):
+    self.id = id
+    self.amount_available = amount_available
+
+  def __repr__(self):
+    return "Equipment(%d, %d)" % (self.id, self.amount_available)
+    
+  def decrease_amount(self, amount):
+    self.amount_available -= amount
+        
+class EquipmentUsage(Base):
+    __tablename__ = 'equipment_usages'
+
+    treatment_id = Column(Integer, ForeignKey('treatments.id'), primary_key=True)
+    equipment_id = Column(Integer, ForeignKey('equipments.id'), primary_key=True)
+    amount_used = Column(Integer, nullable=False)
+    
+    treatment = relationship("Treatment", back_populates="equipment_usages")
+    equipment = relationship("Equipment", back_populates="equipment_usages")
+
+    def __repr__(self):
+        return "EquipmentUsage(%d, %d, %d)" % (self.treatment_id, self.equipment_id, self.amount_used)
+
+engine = create_engine('sqlite:///data.db', echo=False)
+Base.metadata.create_all(engine)
+SessionBase = sessionmaker(bind=engine)
+
+class Session():
+  session = SessionBase()
+  
+  def __get_current_or_none(self):
+    return self.session.query(Treatment).filter_by(is_current=True).first()
+    
+  def __free_current(self):
+    current_treatment = self.__get_current_or_none()
+    if current_treatment is not None:
+      current_treatment.is_current = False
+
+  def __create_new_current_no_free(self):
+    current_treatment = Treatment(is_current=True)
+    self.session.add(current_treatment)
+    return current_treatment
+
+  def create_new_current(self):
+    self.__free_current()
+    return Treatment.__create_new_current_no_free()
+    
+  def get_current_or_create(self):
+    current_treatment = self.__get_current_or_none()
+    if current_treatment is None:
+      return self.__create_new_current_no_free()
+    else:
+      return current_treatment
+  
+  def set_as_current(self, treatment):
+    self.__free_current()
+    treatment.is_current = True
+    
+  def find_equipment(self, equipment_id):
+    return self.session.query(Equipment).filter_by(id=equipment_id).first()
+    
+  def find_equipment_or_create(self, equipment_id):
+    equipment = self.find_equipment(equipment_id)
+    if equipment is None:
+      equipment = Equipment(equipment_id, 100)
+      self.session.add(equipment)
+    return equipment
+    
+  def all_equipment(self):
+    return self.session.query(Equipment).all()
+    
+  def commit(self):
+    self.session.commit()
+    
+  def close(self):
+    self.session.close()
+
+def add_usage(equipment_id, usage):
+  session = Session()
+  equipment = session.find_equipment_or_create(equipment_id)
+  session.get_current_or_create().add_usage(equipment_id, usage)
+  equipment.decrease_amount(usage)
+  session.commit()
+  session.close()
+  
+def update_equipment(equipment_id, amount_available):
+  session = Session()
+  equipment = session.find_equipment(equipment_id)
+  equipment.amount_available = amount_available
+  session.commit()
+  session.close()
+      
