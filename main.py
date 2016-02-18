1dbfd9d325777083464b90e3641e4693c1584cd4
luke725 <luke725@gmail.com>
2016-02-19 00:30:20 +0100

    Load ui file in runtime

--- main.py
+++ main.py
@@ -1,64 +1,62 @@
 import sys
-from PyQt4 import QtCore, QtGui
-from main_window import Ui_MainWindow
+from PyQt4 import QtCore, QtGui, uic
 from model import *
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
-      QtGui.QWidget.__init__(self, parent)
-      self.ui = Ui_MainWindow()
-      self.ui.setupUi(self)
-#      self.ui.equipmentUsedLineEdit.setValidator(QtGui.QIntValidator(0, 99999999999, self))
-      self.ui.addUsageButton.clicked.connect(self.add_usage)
-      self.ui.tableWidget_2.itemChanged.connect(self.change_equipment_amount)
+      super(MainWindow, self).__init__()
+      uic.loadUi('main_window.ui', self)
+#      self.equipmentUsedLineEdit.setValidator(QtGui.QIntValidator(0, 99999999999, self))
+      self.addUsageButton.clicked.connect(self.add_usage)
+      self.tableWidget_2.itemChanged.connect(self.change_equipment_amount)
       
     def change_equipment_amount(self, item):
       if (item.text().toInt()[1]):
         amount_available = item.text().toInt()[0]
-        equipment_id = self.ui.tableWidget_2.item(item.row(), 0).text().toInt()[0]
+        equipment_id = self.tableWidget_2.item(item.row(), 0).text().toInt()[0]
         update_equipment(equipment_id, amount_available)
       self.draw()
       
       
     def add_usage(self):
-      if (self.ui.equipmentUsedLineEdit.text().toInt()[1]):
-        equipment_id = self.ui.equipmentUsedLineEdit.text().toInt()[0]
-        usage = self.ui.amountUsedSpinBox.value()
-        self.ui.equipmentUsedLineEdit.clear()
-        self.ui.amountUsedSpinBox.setValue(1)
+      if (self.equipmentUsedLineEdit.text().toInt()[1]):
+        equipment_id = self.equipmentUsedLineEdit.text().toInt()[0]
+        usage = self.amountUsedSpinBox.value()
         add_usage(equipment_id, usage)
+      self.equipmentUsedLineEdit.clear()
+      self.amountUsedSpinBox.setValue(1)
       self.draw()
 
       
     def __set_row(self, row, key, value):
-      self.ui.tableWidget.setItem(row, 0, self.create_read_only_table_widget_item(str(key)))
-      self.ui.tableWidget.setItem(row, 1, self.create_read_only_table_widget_item(str(value)))
+      self.tableWidget.setItem(row, 0, self.create_read_only_table_widget_item(str(key)))
+      self.tableWidget.setItem(row, 1, self.create_read_only_table_widget_item(str(value)))
       
     def __set_row_2(self, row, key, value):
-      self.ui.tableWidget_2.setItem(row, 0, self.create_read_only_table_widget_item(str(key)))
-      self.ui.tableWidget_2.setItem(row, 1, self.create_table_widget_item(str(value)))
+      self.tableWidget_2.setItem(row, 0, self.create_read_only_table_widget_item(str(key)))
+      self.tableWidget_2.setItem(row, 1, self.create_table_widget_item(str(value)))
       
     def __draw_usage(self, session):
-      self.ui.tableWidget.clearContents()
+      self.tableWidget.clearContents()
       current_treatment = session.get_current_or_create()
       
       row_count = len(current_treatment.equipment_usages)
-      self.ui.tableWidget.setRowCount(row_count)
+      self.tableWidget.setRowCount(row_count)
       for row in range(row_count):
         usage = current_treatment.equipment_usages[row]
         self.__set_row(row, usage.equipment_id, usage.amount_used)
       
     def __draw_state(self, session):
-      self.ui.tableWidget_2.blockSignals(True)
-      self.ui.tableWidget_2.clearContents()
+      self.tableWidget_2.blockSignals(True)
+      self.tableWidget_2.clearContents()
       all_equipment = session.all_equipment()
       
       row_count = len(all_equipment)
-      self.ui.tableWidget_2.setRowCount(row_count)
+      self.tableWidget_2.setRowCount(row_count)
       for row in range(row_count):
         usage = all_equipment[row]
         self.__set_row_2(row, usage.id, usage.amount_available)
-      self.ui.tableWidget_2.blockSignals(False)
+      self.tableWidget_2.blockSignals(False)
       
       
     def draw(self):
--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 from distutils.core import setup
 import py2exe
 
-DATA=[('imageformats',[
-    'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qjpeg4.dll',
-    'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qgif4.dll',
-    'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qico4.dll',
-    'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qmng4.dll',
-    'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qsvg4.dll',
-    'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qtiff4.dll'
-    ])]
+DATA=[
+    ('imageformats',[
+        'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qjpeg4.dll',
+        'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qgif4.dll',
+        'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qico4.dll',
+        'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qmng4.dll',
+        'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qsvg4.dll',
+        'C:\\Python27/Lib/site-packages/PyQt4/plugins/imageformats/qtiff4.dll']),
+    ('', ['main_window.ui'])]
 
 setup(
     windows=['main.py'],
     options={'py2exe':{
-        'includes':['sip'],
+        'includes': ['sip'],
+        'excludes': ['PyQt4.uic.port_v3'],
         'packages': ['sqlalchemy.dialects.sqlite']}},
     data_files = DATA)
